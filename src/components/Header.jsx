9d7ac27eff39a8d873ce3a181a8e5f40ae2d0651
afk-creator <tomas.banys97@gmail.com>
2026-03-22 15:01:52 +0200

    style: update Header design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,8 @@
+// TODO: rewrite this later
 function Header() {
   return (
-    <header style={{ background: '#1e1e2e', color: 'white', padding: '1rem 2rem' }}>
-      <h1>My App</h1>
-      <nav>
-        <a href="/" style={{ color: 'white', marginRight: '1rem' }}>Home</a>
-        <a href="/about" style={{ color: 'white' }}>About</a>
-      </nav>
+    <header style={{ background: 'red', color: 'white', padding: '1rem 2rem' }}>
+      <h1>MY APP!!!</h1>
     </header>
   )
 }
