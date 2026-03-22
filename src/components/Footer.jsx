46694b99d03cb0dfdf43d4fdc4a0d1da1ba99d3c
afk-creator <tomas.banys97@gmail.com>
2026-03-22 15:01:45 +0200

    feat: add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ background: '#1e1e2e', color: 'white', padding: '1rem 2rem', textAlign: 'center' }}>
+      <p>&copy; 2026 My App</p>
+    </footer>
+  )
+}
+
+export default Footer
